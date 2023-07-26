b976710937497a70d355eab1279aa4cf9110e1ba
c-porto <gut0@DESKTOP-37PJH7E>
2023-07-26 17:41:54 -0300

    New Dockerfile and added script

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Running the cosmic2 reader 
+perl ./trigZero2one.pl trgLv0_2023.190.00.04.005.HONDUR_tlm.gz    # its necessary to change the trgLv0 file name to
+                                                                  # the one you need to analize  
